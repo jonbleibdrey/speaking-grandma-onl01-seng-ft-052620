8abcfec7e111d63f6f5b36338189168b1ca46ad0
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-27 01:51:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
   else

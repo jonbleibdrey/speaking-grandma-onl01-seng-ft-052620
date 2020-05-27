5b677f1be224b52db3f91e3296ac8c2446172ac0
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-27 01:54:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(string)
     "HUH?! SPEAK UP, SONNY!"
   elsif string == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
+    elsif
   else
     puts "NO, NOT SINCE 1938!"
   end
